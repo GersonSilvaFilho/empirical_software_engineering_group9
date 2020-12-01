2447ce3795d02e694f6193f2a9fc043bfd93321d
Anton Lutteman <lutteman@student.chalmers.se>
2020-12-01 16:06:34 +0100

    Script file updated

--- lab2/lab2_script_group9.R
+++ lab2/lab2_script_group9.R
@@ -4,7 +4,8 @@ library(knitr)
 library(pwr)
 library(psych)
 library(rmarkdown)
-
+library(zoom)
+library(stargazer)
 
 #a
 # uncertain about the effect size
@@ -82,4 +83,71 @@ plot(tuk)
 # PART 2 Full factorial experiment
 
 # a
+
+# There are four experimental groups assigned to each of the combinations of treatments.
+# The groups consist of the randomly assigned users.
+
+# b
+
+df3 <- read.csv(file = 'gotaflix-2wayANOVA.csv',sep = ",")
+str(df3)
+df3$Engagement <- as.numeric(df3$Engagement)
+df3$Cover <- as.factor(df3$Cover)
+df3$Summary <- as.factor(df3$Summary)
+
+# Intercept for lm3 and lm4 is for factors Cover = Character and Summary = Character
+lm3 <- lm(Engagement ~ Cover + Summary, df3)
+
+
+lm4 <- lm(Engagement ~ Cover + Summary + Cover:Summary, df3)
+lm5 <- lm(Engagement ~ Cover*Summary, df3)
+
+# c
+
+# The samples were taken randomly so the assumption of independence holds
+
+
+# The shapiro wilks test with W=0.999 with a p value of 0.3843, therefore we cannot reject the 
+# null hypothesis
+shapiro.test(df3$Engagement)
+
+# The results of the levene test tells us that we can reject the null hypothesis that the data is
+# homoscedastic, however with large sample sizes the test is likely to report small p values for
+# very small deviations from homoscedasticity. Instead we use qqPlot on the resiudals to inspect
+# homoscedasticity. Our conclusion is that we cannot reject the hypothesis that the data is homoscedastic.
+car::leveneTest(lm5)
+car::qqPlot(sample(lm5$residuals,100))
+
+
+# d
+# The statisticly significant result is that The combination of Character for both Genre and Summary
+# has better engagement than all other combinations of Genre and Summary. 
+
+anovatwoway <- car::Anova(lm5)
+tuk <- TukeyHSD(aov(lm5))
+plot(tuk)
+
+# The effect size 
+pwr.anova.test(k = 4, n = 800, f = NULL, sig.level = 0.05, power = 0.90)
+#render('lab2_report_group9.Rmd')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
